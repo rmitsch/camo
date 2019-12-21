f8ebab3fbc2598352fe555fc20c8d3b463d5a7e7
Raphael Mitsch <laarem33@hotmail.com>
2019-12-21 13:41:13 +0100

    Added chart for community balance. Minor refactoring.

--- source/app.py
+++ source/app.py
@@ -5,57 +5,40 @@ todo:
     - Mixed layout (when supported by streamlit).
 """
 
+
 import pandas as pd
 import streamlit as st
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
+from typing import Tuple, List
 import sys
 sys.path.append('../')
 import source.utils as utils
-from source.fileparsing import ODSReader
 
 
 st.title("Batcave Cashflow")
 st.sidebar.title("Settings")
 
-# Read entries from spreadsheet, add artifical data points for investment so that line is drawn from beginning to end
-# of plot.
-entries: pd.DataFrame = utils.add_investment_fillers(ODSReader("/home/raphael/Documents/Finanzen/Cashflow.ods").entries)
+# Load data.
+loaded_data: Tuple[pd.DataFrame, List[str]] = utils.load_data("/home/raphael/Documents/Finanzen/Cashflow.ods")
+entries: pd.DataFrame = loaded_data[0]
+users: List[str] = loaded_data[1]
 
-# Gather all users (assuming that each user paid at least once).
-users: list = entries.From.unique().tolist()
-
-# One-hot encode beneficiaries.
-entries["n_beneficiaries"] = entries.To.str.split(",").apply(len)
-for user in users:
-    entries["amount_to_" + user] = entries.To.str.contains(user) * entries.Amount / entries.n_beneficiaries
-
-# Sum values per day and category. Ignore amortization entries for time chart, since they are only interesting for the
-# community balance calculation.
-entries_cumulative: pd.DataFrame = entries[
-    entries.Category != "Amortization"
-].groupby(["Date", "Category"]).sum().reset_index()
-entries_cumulative.Date = pd.to_datetime(entries_cumulative.Date)
-investment_entry_idx: pd.Series = (entries_cumulative.Category == "Investment")
-
-# Assemble dataframe for cumulative sum of investment and non-investement data.
-entries_cumulative = pd.concat([
-    # Investment is treated as special kind expense, but here we want it to be displayed as positive in the charts.
-    entries_cumulative[investment_entry_idx].set_index(["Category", "Date"])[["Amount"]].cumsum() * -1,
-    # Everything but investment and amortization, since they are to be treated differently.
-    entries_cumulative[~investment_entry_idx].set_index(["Category", "Date"])[["Amount"]].cumsum()
-]).reset_index()
-
-# Assign color values for series.
-entries_cumulative["color"] = (entries_cumulative.Category == "Investment").replace({True: "Orange", False: "Blue"})
+# Get timeseries data.
+entries_cumulative: pd.DataFrame = utils.compute_liquidity_timeseries(entries)
 
 # Plot cashflow.
 plot: plotly.graph_objects.Figure = px.line(
-    entries_cumulative, x="Date", y="Amount", title='', color="color", line_shape="hvh"
+    entries_cumulative,
+    x="Date",
+    y="Amount",
+    title="Liquidity and Investment Over Time",
+    color="color",
+    line_shape="hvh"
 )
 plot.update_layout(
-    margin=dict(l=0, r=0, t=0, b=0),
+    margin=dict(l=0, r=0, t=30, b=0),
     legend=dict(x=0, y=0)
 )
 plot.update_yaxes(range=[entries_cumulative.min().Amount * 1.1, entries_cumulative.max().Amount * 1.1])
@@ -69,10 +52,6 @@ community_entries: pd.DataFrame = entries[
     (entries.Category != "Amortization") &
     ((entries.n_beneficiaries > 1) | (~entries.From.isin(entries.To)))
 ]
-st.write("amort")
-st.write(amortizations)
-st.write("comm entrei")
-st.write(community_entries)
 
 # Compute all sums that were paid _by_ given user. Assuming payer is only ever a single person.
 community_balances: dict = community_entries.groupby("From").sum()[["Amount"]].to_dict()["Amount"]
@@ -86,4 +65,11 @@ for ix, row in amortizations.groupby(["From", "To"]).sum()[["Amount"]].iterrows(
     community_balances[ix[0]] -= row.Amount
     community_balances[ix[1]] += row.Amount
 
-st.write("Community balance:", community_balances)
+plot = go.Figure([go.Bar(x=users, y=[community_balances[user] for user in users])])
+plot.update_layout(
+    title="Community Balance",
+    xaxis_title="",
+    yaxis_title="€",
+    margin=dict(l=0, r=0, t=30, b=0)
+)
+st.plotly_chart(plot, width=200, height=200)
--- source/utils.py
+++ source/utils.py
@@ -1,4 +1,59 @@
 import pandas as pd
+from typing import Tuple, List
+from source.fileparsing import ODSReader
+
+
+def load_data(spreadsheet_path: str) -> Tuple[pd.DataFrame, List[str]]:
+    """
+    Loads and parses spreadsheet data.
+    :param spreadsheet_path:
+    :type spreadsheet_path:
+    :return: Parsed entries in spreadsheet, list of user names/abbreviations.
+    :rtype: Tuple[pd.DataFrame, List[str]]
+    """
+    # Load and parse spreadsheet.
+    entries: pd.DataFrame = add_investment_fillers(ODSReader(spreadsheet_path).entries)
+
+    # Gather all users (assuming that each user paid at least once).
+    users: list = entries.From.unique().tolist()
+
+    # One-hot encode beneficiaries.
+    entries["n_beneficiaries"] = entries.To.str.split(",").apply(len)
+    for user in users:
+        entries["amount_to_" + user] = entries.To.str.contains(user) * entries.Amount / entries.n_beneficiaries
+
+    return entries, users
+
+
+def compute_liquidity_timeseries(entries: pd.DataFrame) -> pd.DataFrame:
+    """
+    Computes cumulative sums for liquidity/investment timeseries chart.
+    :param entries:
+    :type entries:
+    :return:
+    :rtype:
+    """
+
+    # Sum values per day and category. Ignore amortization entries for time chart, since they are only interesting for
+    # the community balance calculation.
+    entries_cumulative: pd.DataFrame = entries[
+        entries.Category != "Amortization"
+        ].groupby(["Date", "Category"]).sum().reset_index()
+    entries_cumulative.Date = pd.to_datetime(entries_cumulative.Date)
+    investment_entry_idx: pd.Series = (entries_cumulative.Category == "Investment")
+
+    # Assemble dataframe for cumulative sum of investment and non-investement data.
+    entries_cumulative = pd.concat([
+        # Investment is treated as special kind expense, but here we want it to be displayed as positive in the charts.
+        entries_cumulative[investment_entry_idx].set_index(["Category", "Date"])[["Amount"]].cumsum() * -1,
+        # Everything but investment and amortization, since they are to be treated differently.
+        entries_cumulative[~investment_entry_idx].set_index(["Category", "Date"])[["Amount"]].cumsum()
+    ]).reset_index()
+
+    # Assign color values for series.
+    entries_cumulative["color"] = (entries_cumulative.Category == "Investment").replace({True: "Orange", False: "Blue"})
+
+    return entries_cumulative
 
 
 def add_investment_fillers(entries: pd.DataFrame) -> pd.DataFrame:
